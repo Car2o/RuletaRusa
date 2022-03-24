199a6094dd2a737bae73f93a51aba29b8e1dc6e5
Daniel0Saez <danielsaez@iescastelar.com>
2022-03-24 12:52:55 +0100

    1ercom

---
+++ src/RuletaRusa/Juego.java
@@ -0,0 +1,4 @@
+package RuletaRusa;
+
+public class Juego {
+}
---
+++ src/RuletaRusa/Revolver.java
@@ -0,0 +1,45 @@
+package RuletaRusa;
+import java.util.Random;
+public class Revolver {
+
+    //atributos del revolver
+    protected int tambor;
+    protected int posicion;
+    protected int bala;
+    //atributos para el random
+    Random random = new Random();
+    private int min =1;
+    private int max = 6;
+
+    //constructor
+    Revolver(){
+        generarBala();
+        posicion=0;
+        tambor=6;
+    }
+
+    //get
+
+    public int getTambor() {
+        return tambor;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    private int generarBala(){
+        bala = random.nextInt(min + max)+min;
+        return bala;
+    }
+
+    private int disparar(){
+        posicion ++;
+        if(posicion == bala){
+            System.out.println("BANG");
+        } else{
+            System.out.println("Vives por ahora");
+        }
+        return posicion;
+    }
+}
